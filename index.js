84d7983a2a6b4c29e650b4062b8d46e7782ea95c
Hamzah Contreras <Hamzah_36@outlook.com>
2021-10-21 10:54:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "Semsem is the champion";
+
+document.body.append(newHeader);
