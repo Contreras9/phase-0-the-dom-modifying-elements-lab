637eafa463ed6956c3b4faeb4e3bde89f7e89ba6
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-18 22:00:17 -0600

    commit tree fd06284175addbf7092dcde1bacf6ac6b583eaca

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// Write your code here!
+// // Write your code here!
 
 document.body.removeChild(main);
 
 const newHeader = document.createElement("h1");
+
 newHeader.id = "victory";
 
 newHeader.textContent = "Semsem is the champion";
